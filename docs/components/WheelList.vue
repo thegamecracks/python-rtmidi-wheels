1b0a1c452e4cfc9246e58dfbd34949dad16f2aa5
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-12-09 22:02:55 -0500

    Add nuxt site for statically displaying wheel downloads

---
+++ docs/components/WheelList.vue
@@ -0,0 +1,27 @@
+<script setup>
+const wheelData = await fetch(
+    "https://api.github.com/repos/thegamecracks/python-rtmidi-wheels/contents/dist",
+    {
+        headers: {
+            Accept: "application/vnd.github+json",
+        },
+    },
+).then((response) => response.json())
+</script>
+
+<template>
+    <h1>Wheels for sale!</h1>
+    <ul class="no-dots">
+        <li v-for="wheel in wheelData" :key="wheel.name">
+            <a :href="wheel.download_url">
+                {{ wheel.name }}
+            </a>
+        </li>
+    </ul>
+</template>
+
+<style>
+.no-dots {
+    list-style-type: none;
+}
+</style>
